ca265ee8b49d4d5ab3020bd789d2380cece42885
That Mr.L <thatmr.l@gmail.com>
2018-05-13 21:34:40 +0800

    feat(windows): Introduce a script for Windows

---
+++ windows/run.cmd
@@ -0,0 +1,19 @@
+rem Install the package manager
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+rem Essential
+choco install 7zip -y
+choco install everything -y
+choco install firefox -y
+choco install geekuninstaller -y
+choco install libreoffice -y
+choco install shadowsocks -y
+choco install sumatrapdf -y
+choco install wox -y
+
+rem Development
+choco install androidstudio -y
+choco install autohotkey -y
+choco install git -y
+choco install sublimetext3 -y
+choco install zeal -y
